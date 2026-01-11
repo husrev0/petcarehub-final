ed8122117012b2c1397405c8e5005d59a710db17
husrev0 <aak40475@gmail.com>
2026-01-11 16:43:35 +0300

    temizlik

--- Inbox.jsx
+++
@@ -1,3 +0,0 @@
-export default function Inbox() {
-    return <div className="p-4"><h1>Inbox</h1></div>;
-}
--- LandingPage.jsx
+++
@@ -1,3 +0,0 @@
-export default function LandingPage() {
-    return <div className="p-4"><h1>Welcome to PetCareHub</h1><p>Find the best sitter for your pet.</p></div>;
-}
