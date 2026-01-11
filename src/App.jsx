69fba5466670954a9c8794f3e32fbae98e7a311e
husrev0 <aak40475@gmail.com>
2026-01-11 22:14:23 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import { Routes, Route } from 'react-router-dom'
 
-// Bunlar 'pages' klasöründeyse böyle kalsın:
 import LandingPage from './pages/LandingPage'
 import Login from './pages/Login'
 import Register from './pages/Register'
-
-// DİKKAT: Resimde MyBookings.jsx 'src' içinde görünüyor (App.jsx yaninda).
-// O yüzden başındaki 'pages/' kısmını sildik:
-import MyBookings from './MyBookings' 
+import MyBookings from './MyBookings'
 
 export default function App() {
   return (
